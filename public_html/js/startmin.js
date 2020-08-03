a39ee4a53fbeb8c27c3c3fa258217485d270a606
TRFN <tulio.nasc95@gmail.com>
2020-08-02 23:18:03 -0300

    Ajuste CSS Menu

--- public_html/js/startmin.js
+++ public_html/js/startmin.js
@@ -1,4 +1,4 @@
-$(function() {
+try{ $(function() {
 
     $('#side-menu').metisMenu();
 
@@ -8,7 +8,7 @@ $(function() {
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
-    $(window).bind("load resize", function() {
+    $(window).bind("load resize", e=function() {
         topOffset = 50;
         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
@@ -26,6 +26,8 @@ $(function() {
         }
     });
 
+    e();
+
     var url = window.location;
     var element = $('ul.nav a').filter(function() {
         return this.href == url || url.href.indexOf(this.href) == 0;
@@ -33,4 +35,4 @@ $(function() {
     if (element.is('li')) {
         element.addClass('active');
     }
-});
+});} catch(e){alert(e);}
